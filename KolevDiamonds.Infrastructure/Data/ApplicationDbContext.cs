81777f80dc9f807bc3d3bf32fc51bd2f9250b959
100QN <83006473+100QNKolev@users.noreply.github.com>
2024-03-09 15:40:07 +0200

    Updated DbContext for the new classes

--- KolevDiamonds.Infrastructure/Data/ApplicationDbContext.cs
+++ KolevDiamonds.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using KolevDiamonds.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace KolevDiamonds.Data
@@ -8,6 +9,17 @@ namespace KolevDiamonds.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Ring> Rings { get; set; }
+        public DbSet<Necklace> Necklaces { get; set; }
+        public DbSet<InvestmentDiamond> investmentDiamonds { get; set; }
+        public DbSet<InvestmentCoin> investmentCoins { get; set; }
+        public DbSet<MetalBar> metalBars { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
         }
     }
 }
