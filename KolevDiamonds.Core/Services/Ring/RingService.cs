df986b645df460390b9d53c305675d69aa28414a
100QN <83006473+100QNKolev@users.noreply.github.com>
2024-03-09 20:57:11 +0200

    GitHub Bug
    Need to commit

---
+++ KolevDiamonds.Core/Services/Ring/RingService.cs
@@ -0,0 +1,30 @@
+﻿using KolevDiamonds.Core.Contracts.Ring;
+using KolevDiamonds.Core.Models;
+using KolevDiamonds.Infrastructure.Data;
+using KolevDiamonds.Infrastructure.Data.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace KolevDiamonds.Core.Services.Ring
+{
+    public class RingService : IRingService
+    {
+        private readonly IRepository _repository;
+
+        public RingService(IRepository repository)
+        {
+            this._repository = repository;
+        }
+
+        public async Task<IEnumerable<RingIndexServiceModel>> AllRings()
+        {
+            return await this._repository
+                .AllReadOnly<Infrastructure.Data.Models.Ring>()
+                .OrderByDescending(r => r.RingId)
+                .Select(r => new RingIndexServiceModel() 
+                {
+                
+                })
+                .ToListAsync();
+        }
+    }
+}
---
+++ KolevDiamonds.Infrastructure/Data/Common/Repository.cs
@@ -0,0 +1,32 @@
+﻿
+using KolevDiamonds.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace KolevDiamonds.Infrastructure.Data.Common
+{
+    public class Repository : IRepository
+    {
+        private readonly DbContext _context;
+
+        public Repository(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        private DbSet<T> DbSet<T> () where T : class
+        {
+            return this._context.Set<T>();
+        }
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>();
+        }
+
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return DbSet<T>()
+                .AsNoTracking();
+        }
+    }
+}
