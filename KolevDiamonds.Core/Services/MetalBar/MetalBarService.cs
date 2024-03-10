29b3c75e1e35d9d1256953685224e467c958bd03
100QN <83006473+100QNKolev@users.noreply.github.com>
2024-03-10 14:40:02 +0200

    Added metal bar service

---
+++ KolevDiamonds.Core/Services/MetalBar/MetalBarService.cs
@@ -0,0 +1,37 @@
+﻿using KolevDiamonds.Core.Contracts.MetalBar;
+using KolevDiamonds.Core.Models;
+using KolevDiamonds.Infrastructure.Data.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KolevDiamonds.Core.Services.MetalBar
+{
+    public class MetalBarService : IMetalBarService
+    {
+        private readonly IRepository _repository;
+
+        public MetalBarService(IRepository repository)
+        {
+            this._repository = repository;
+        }
+
+        public async Task<IEnumerable<ProductIndexServiceModel>> AllMetalBars()
+        {
+            return await this._repository
+                .AllReadOnly<Infrastructure.Data.Models.MetalBar>()
+                .OrderByDescending(r => r.BarId)
+                .Select(r => new ProductIndexServiceModel()
+                {
+                    Id = r.BarId,
+                    Name = r.MetalBarName,
+                    ImagePath = r.MetalBarImagePath,
+                    Price = r.Price
+                })
+                .ToListAsync();
+        }
+    }
+}
