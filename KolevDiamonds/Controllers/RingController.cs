6db329306c532781f2d4035ccb547bb73fd099ca
100QN <83006473+100QNKolev@users.noreply.github.com>
2024-03-12 12:09:23 +0200

    Created controller for rings

---
+++ KolevDiamonds/Controllers/RingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KolevDiamonds.Controllers
+{
+    public class RingController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+
+
+            return View();
+        }
+    }
+}
