b7e0fc2d3000edbcf7569750640e7bda5de16a2c
100QN <83006473+100QNKolev@users.noreply.github.com>
2024-03-12 14:15:31 +0200

    Added details model for InvestmentDiamondService

---
+++ KolevDiamonds.Core/Models/InvestmentDiamond/InvestmentDiamondDetailsServiceModel.cs
@@ -0,0 +1,34 @@
+﻿using KolevDiamonds.Infrastructure.Enums;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KolevDiamonds.Core.Models.InvestmentDiamond
+{
+    public class InvestmentDiamondDetailsServiceModel
+    {
+        public int DiamondId { get; set; }
+
+        public string DiamondName { get; set; } = string.Empty;
+
+        public string DiamondImagePath { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+
+        public double Carats { get; set; }
+
+        public DiamondColor Colour { get; set; }
+
+        public DiamondClarity Clarity { get; set; }
+
+        public DiamondCut Cut { get; set; }
+
+        public string CertifyingLaboratory { get; set; } = string.Empty;
+
+        public string Proportions { get; set; } = string.Empty;
+    }
+}
