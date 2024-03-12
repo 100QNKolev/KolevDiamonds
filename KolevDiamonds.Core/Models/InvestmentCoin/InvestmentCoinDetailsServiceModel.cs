3102c9b796fb19443c039ed360df8fe07d09e333
100QN <83006473+100QNKolev@users.noreply.github.com>
2024-03-12 14:18:04 +0200

    Added details model for investment coin service

---
+++ KolevDiamonds.Core/Models/InvestmentCoin/InvestmentCoinDetailsServiceModel.cs
@@ -0,0 +1,38 @@
+﻿using KolevDiamonds.Infrastructure.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KolevDiamonds.Core.Models.InvestmentCoin
+{
+    public class InvestmentCoinDetailsServiceModel
+    {
+        public int CoinId { get; set; }
+
+        public string CoinName { get; set; } = string.Empty;
+
+        public string CoinImagePath { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+
+        public MetalVariation Metal { get; set; }
+
+        public double Weight { get; set; }
+
+        public double Purity { get; set; }
+
+        public GoldQuality Quality { get; set; }
+
+        public int Circulation { get; set; }
+
+        public double Diameter { get; set; }
+
+        public string LegalTender { get; set; } = string.Empty;
+
+        public string Manufacturer { get; set; } = string.Empty;
+
+        public string Packaging { get; set; } = string.Empty;
+    }
+}
