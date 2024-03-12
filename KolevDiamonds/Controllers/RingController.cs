350c18f60de9cbbe1e63b828ebd7b844091c72e0
100QN <83006473+100QNKolev@users.noreply.github.com>
2024-03-12 12:49:10 +0200

    Added base index view for rings

--- KolevDiamonds/Controllers/RingController.cs
+++ KolevDiamonds/Controllers/RingController.cs
@@ -1,15 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KolevDiamonds.Core.Contracts.Ring;
+using KolevDiamonds.Core.Models;
+using KolevDiamonds.Core.Services.Ring;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KolevDiamonds.Controllers
 {
     public class RingController : Controller
     {
+        private readonly IRingService _ringService;
+
+        public RingController(IRingService ringService)
+        {
+            this._ringService = ringService;
+        }
+
         [HttpGet]
         public async Task<IActionResult> Index()
         {
+            var model = await this._ringService
+                .AllRings();
 
+            ViewBag.PriceFilter = 0;
 
-            return View();
+            return View(model);
         }
     }
 }
