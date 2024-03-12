f26ddfa6815d903472bab42d2825de96121a8eb1
100QN <83006473+100QNKolev@users.noreply.github.com>
2024-03-12 13:01:25 +0200

    Added service model for ring get by id

---
+++ KolevDiamonds.Core/Models/Ring/RingDetailsServiceModel.cs
@@ -0,0 +1,34 @@
+﻿using KolevDiamonds.Infrastructure.Enums;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KolevDiamonds.Core.Models.Ring
+{
+    public class RingDetailsServiceModel
+    {
+        public int RingId { get; set; }
+
+        public string RingName { get; set; } = string.Empty;
+
+        public string RingImagePath { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+
+        public MetalVariation Metal { get; set; }
+
+        public double Carats { get; set; }
+
+        public DiamondColor Colour { get; set; }
+
+        public DiamondClarity Clarity { get; set; }
+
+        public DiamondCut Cut { get; set; }
+
+        public string Purity { get; set; } = string.Empty;
+    }
+}
