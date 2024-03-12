7f5d8e1119952df182b0805ad0360486e71d9c91
100QN <83006473+100QNKolev@users.noreply.github.com>
2024-03-12 13:57:28 +0200

    Added details model for metal bar service

---
+++ KolevDiamonds.Core/Models/MetalBar/MetalBarDetailsServiceModel.cs
@@ -0,0 +1,28 @@
+﻿using KolevDiamonds.Infrastructure.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KolevDiamonds.Core.Models.MetalBar
+{
+    public class MetalBarDetailsServiceModel
+    {
+        public int BarId { get; set; }
+
+        public string MetalBarName { get; set; } = string.Empty;
+
+        public string MetalBarImagePath { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+
+        public MetalVariation Metal { get; set; }
+
+        public double Weight { get; set; }
+
+        public string Dimensions { get; set; } = string.Empty;
+
+        public string Purity { get; set; } = string.Empty;
+    }
+}
