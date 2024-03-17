e6ab47bf9515382d9e4c5d850f90c88c604c4c46
100QN <83006473+100QNKolev@users.noreply.github.com>
2024-03-17 11:07:32 +0200

    Created cart controller

---
+++ KolevDiamonds/Controllers/CartController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KolevDiamonds.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CartController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public IActionResult Index()
+        {
+            List<string> cartItems = GetCartItems();
+            return View(cartItems);
+        }
+
+        public IActionResult AddToCart(int productId)
+        {
+            List<string> cartItems = GetCartItems();
+            cartItems.Add(productId.ToString());
+            SaveCartItems(cartItems);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult RemoveFromCart(int productId)
+        {
+            List<string> cartItems = GetCartItems();
+            cartItems.Remove(productId.ToString());
+            SaveCartItems(cartItems);
+            return RedirectToAction("Index");
+        }
+
+        private List<string> GetCartItems()
+        {
+            var cartItemsString = _httpContextAccessor.HttpContext?.Session.GetString("CartItems");
+            if (cartItemsString == null)
+            {
+                return new List<string>();
+            }
+            return cartItemsString.Split(',').ToList();
+        }
+
+        private void SaveCartItems(List<string> cartItems)
+        {
+            _httpContextAccessor.HttpContext?.Session.SetString("CartItems", string.Join(",", cartItems));
+        }
+    }
+}
