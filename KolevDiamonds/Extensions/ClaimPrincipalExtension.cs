1ff54b0a116353e57102e9c0f4094ba24b02a899
100QN <83006473+100QNKolev@users.noreply.github.com>
2024-04-05 16:53:53 +0300

    Added IsAdmin claim

---
+++ KolevDiamonds/Extensions/ClaimPrincipalExtension.cs
@@ -0,0 +1,12 @@
+﻿using static KolevDiamonds.Infrastructure.Data.Models.AdminUser;
+
+namespace System.Security.Claims
+{
+    public static class ClaimPrincipalExtension
+    {
+        public static bool isAdmin(this ClaimsPrincipal user) 
+        {
+            return user.IsInRole(AdminRoleName);
+        }
+    }
+}
