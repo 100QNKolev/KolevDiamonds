4253247e4c3bafd313d48a18fc8f0b7395632de2
100QN <83006473+100QNKolev@users.noreply.github.com>
2024-04-12 14:59:23 +0300

    Added ring model for validation on adding new entity

---
+++ KolevDiamonds.Core/Models/Ring/RingModel.cs
@@ -0,0 +1,42 @@
+﻿using KolevDiamonds.Infrastructure.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static KolevDiamonds.Infrastructure.Constants.DataConstants;
+
+namespace KolevDiamonds.Core.Models.Ring
+{
+    public class RingModel
+    {
+        [Required]
+        [StringLength(RingNameMaximumLength, MinimumLength = RingNameMinimumLength)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string ImagePath { get; set; } = string.Empty;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public MetalVariation Metal { get; set; }
+
+        [Required]
+        public double Carats { get; set; }
+
+        [Required]
+        public DiamondColor Colour { get; set; }
+
+        [Required]
+        public DiamondClarity Clarity { get; set; }
+
+        [Required]
+        public DiamondCut Cut { get; set; }
+
+        [Required]
+        [StringLength(RingPurityMaximumLength, MinimumLength = RingPurityMinumumLength)]
+        public string Purity { get; set; } = string.Empty;
+
+        [Required]
+        public bool IsForSale { get; set; } = true;
+    }
+}
