0868753bc2c7974f6eb39aff575215ad32222f98
100QN <83006473+100QNKolev@users.noreply.github.com>
2024-04-12 15:03:44 +0300

    Added metal bar model for validation on adding new entity

---
+++ KolevDiamonds.Core/Models/MetalBar/MetalBarModel.cs
@@ -0,0 +1,36 @@
+﻿using KolevDiamonds.Infrastructure.Enums;
+using System.ComponentModel.DataAnnotations;
+using static KolevDiamonds.Infrastructure.Constants.DataConstants;
+
+namespace KolevDiamonds.Core.Models.MetalBar
+{
+    public class MetalBarModel
+    {
+        [Required]
+        [StringLength(MetalBarNameMaximumLength, MinimumLength = MetalBarNameMinimumLength)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string ImagePath { get; set; } = string.Empty;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public MetalVariation Metal { get; set; }
+
+        [Required]
+        public double Weight { get; set; }
+
+        [Required]
+        [StringLength(MetalBarDimentionsMaximumLength, MinimumLength = MetalBarDimentionsMinumumLength)]
+        public string Dimensions { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(MetalBarPurityMaximumLength, MinimumLength = MetalBarPurityMinumumLength)]
+        public string Purity { get; set; } = string.Empty;
+
+        [Required]
+        public bool IsForSale { get; set; } = true;
+    }
+}
